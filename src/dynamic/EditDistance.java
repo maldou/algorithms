04ed510d760d706f79cbe2e8bceb23fb7903412a
maldou <mmengzi@gmail.com>
2016-07-10 18:40:07 +0800

    编辑距离

---
+++ src/dynamic/EditDistance.java
@@ -0,0 +1,124 @@
+package dynamic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/**
+ * 编辑距离，动态规划
+ * @author waldoudou
+ *
+ * @param <E>
+ */
+public class EditDistance<E> {
+	
+	private int updateCost = 1;
+	private int insertCost = 1;
+	private int deleteCost = 1;
+	
+	public EditDistance() {
+		this(1, 1, 1);
+	}
+	public EditDistance(int updateCost, int insertCost, int deleteCost) {
+		this.updateCost = updateCost;
+		this.insertCost = insertCost;
+		this.deleteCost = deleteCost;
+	}
+	
+	public int editDistance(List<E> list1, List<E> list2) {
+		int[][] editArray = editArray(list1, list2);
+		printEditArray(editArray, list1.size() + 1, list2.size() + 1);
+        int distance = editArray[list1.size()][list2.size()];
+        return distance;
+	}
+	
+	private int[][] editArray(List<E> list1, List<E> list2) {
+		int[][] editArray = new int[list1.size() + 1][list2.size() + 1];
+		//E（0，0）= 0，E（i，0）= i，E（0，j）= j
+		for(int i = 0; i <= list1.size(); i++) {
+			editArray[i][0] = i;
+		}
+		for(int i = 0; i <= list2.size(); i++) {
+			editArray[0][i] = i;
+		}
+		//E(i, j) = min( [E(i-1, j) + D], [E(i, j-1) + I],  [E(i-1, j-1) + R (如果 i,j 字符不一样)] )
+		for(int i = 1; i <= list1.size(); i++) {
+			E e1 = list1.get(i - 1);
+			for(int j = 1; j <= list2.size(); j++) {
+				E e2 = list2.get(j - 1);
+				int min = Math.min(editArray[i - 1][j] + deleteCost , editArray[i][j - 1] + insertCost);
+				int r = e1.equals(e2) ? 0 : updateCost;
+				min = Math.min(min, editArray[i - 1][j- 1] + r);
+				editArray[i][j] = min;
+			}
+		}
+		
+		return editArray;
+	}
+	
+	private void printEditArray(int[][] editArray, int length1, int length2) {
+		System.out.println("========edit array=====================");
+        for(int i = 0; i < length1; i++) {
+            for(int j = 0; j < length2; j++) {
+                System.out.print(editArray[i][j] + "  ");
+            }
+            System.out.println("");
+        }
+	}
+	
+	public static void main(String[] args) {
+		String[] arr1 = new String[]{"b", "r", "o", "t", "h", "e", "r"}; //brother;
+		List<String> list1 = Arrays.asList(arr1);
+		String[] arr2 = new String[]{"f", "a", "t", "h", "e", "r"}; //father
+		List<String> list2 = Arrays.asList(arr2);
+		
+		EditDistance<String> ed = new EditDistance<>();
+		int distance = ed.editDistance(list1, list2);
+		System.out.println("edit distance:" + distance);
+		
+		EditDistance<Entity> ed2 = new EditDistance<>();
+		Entity e1 = new Entity(1);
+		Entity e2 = new Entity(3);
+		Entity e3 = new Entity(10);
+		Entity e4 = new Entity(9);
+		Entity e5 = new Entity(8);
+		Entity e6 = new Entity(5);
+		Entity e7 = new Entity(6);
+		Entity e8 = new Entity(15);
+		Entity e9 = new Entity(23);
+		Entity e10 = new Entity(24);
+		Entity e11 = new Entity(91);
+		Entity e12 = new Entity(19);
+		List<Entity> liste1 = new ArrayList<>();
+		liste1.add(e1);
+		liste1.add(e12);
+		liste1.add(e5);
+		liste1.add(e8);
+		liste1.add(e10);
+		List<Entity> liste2 = new ArrayList<>();
+		liste2.add(e12);
+		liste2.add(e8);
+		liste2.add(e10);
+		liste2.add(e2);
+		liste2.add(e3);
+		distance = ed2.editDistance(liste1, liste2);
+		System.out.println("edit distance:" + distance);
+	}
+}
+
+class Entity {
+	private int index;
+	
+	public Entity(int index) {
+		this.index = index;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if(other instanceof Entity) {
+			Entity e = (Entity)other;
+			return e.index == index;
+		}
+		return false;
+	}
+	
+}
--- src/dynamic/LCSubsequence.java
+++ src/dynamic/LCSubsequence.java
@@ -1,6 +1,7 @@
 package dynamic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /** 
@@ -8,35 +9,22 @@ import java.util.List;
  * @author waldoudou
  *
  */
-public class LCSubsequence {
+public class LCSubsequence<E> {
 	
-	public String[] lcs(String[] strArray1, String[] strArray2) {
-		String[] newStrArray1 = transStr(strArray1);
-		String[] newStrArray2 = transStr(strArray2);
-		int[][] b = lcsLength(newStrArray1, newStrArray2);
-		List<String> result = new ArrayList<String>();
-		getLcs(b, newStrArray1, newStrArray1.length - 1, newStrArray2.length - 1, result);
-		
-		return result.toArray(new String[result.size()]);
+	public List<E> lcs(List<E> list1, List<E> list2) {
+		int[][] b = lcsLength(list1, list2);
+		List<E> result = new ArrayList<>();
+		getLcs(b, list1, list1.size(), list2.size(), result);
+		return result;
 	}
 	
-	private String[] transStr(String[] array) {
-		int length1 = array.length;
-		String[] newArray = new String[length1 + 1];
-		newArray[0] = "";
-		for(int i = 0; i < length1; i++) {
-			newArray[i + 1] = array[i];
-		}
-		return newArray;
-	}
-	
-	private int[][] lcsLength(String[] strArray1, String[] strArray2) {
-		int[][] b = new int[strArray1.length][strArray2.length];
-		int[][] c = new int[strArray1.length][strArray2.length];
-		for(int i = 1; i < strArray1.length; i++) {
-			for(int j = 1; j < strArray2.length; j++) {
+	private int[][] lcsLength(List<E> list1, List<E> list2) {
+		int[][] b = new int[list1.size() + 1][list2.size() + 1];
+		int[][] c = new int[list1.size() + 1][list2.size() + 1];
+		for(int i = 1; i < list1.size() + 1; i++) {
+			for(int j = 1; j < list2.size() + 1; j++) {
 				//c[i,j] = c[i - 1, j - 1] + 1
-				if(strArray1[i].equals(strArray2[j])) {
+				if(list1.get(i - 1).equals(list2.get(j - 1))) {
 					c[i][j] = c[i - 1][j - 1] + 1;
 					b[i][j] = 0; // 左上
 				}
@@ -56,29 +44,37 @@ public class LCSubsequence {
 		return b;
 	}
 	
-	private void getLcs(int[][] b, String[] strArray, int i, int j, List<String> result) {
+	private void getLcs(int[][] b, List<E> list, int i, int j, List<E> result) {
 		if(i == 0 || j == 0) {
 			return ;
 		}
 		if(b[i][j] == 0) {
-			getLcs(b, strArray, i - 1, j - 1, result);
-			result.add(strArray[i]);
+			getLcs(b, list, i - 1, j - 1, result);
+			result.add(list.get(i - 1));
 		}
 		else if(b[i][j] == -1) {
-			getLcs(b, strArray, i - 1, j, result);
+			getLcs(b, list, i - 1, j, result);
 		}
 		else {
-			getLcs(b, strArray, i, j - 1, result);
+			getLcs(b, list, i, j - 1, result);
 		}
 	}
 	
 	public static void main(String[] args) {
 		String[] array1 = new String[]{"a", "b", "c", "d", "e", "f", "g"};
 		String[] array2 = new String[]{"a", "c", "a", "b", "f", "c", "e", "h"};
-		LCSubsequence lcs = new LCSubsequence();
-		String[] result = lcs.lcs(array1, array2);
+		LCSubsequence<String> lcs = new LCSubsequence<>();
+		List<String> result = lcs.lcs(Arrays.asList(array1), Arrays.asList(array2));
 		for(String str : result) {
 			System.out.print(str);
+		}
+		System.out.println();
+		array1 = new String[]{"中国", "美国", "法国", "德国", "英国", "意大利", "冰岛"};
+		array2 = new String[]{"中国", "日本", "美国", "德国", "法国", "捷克", "英国", "冰岛"};
+		lcs = new LCSubsequence<>();
+		result = lcs.lcs(Arrays.asList(array1), Arrays.asList(array2));
+		for(String str : result) {
+			System.out.print(str + ";");
 		}
 	}
 }
